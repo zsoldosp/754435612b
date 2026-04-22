247b07b65388e20a711e5acd7489df380a2f0c1a
Peter Zsoldos <hello@zsoldosp.eu>
2026-04-22 10:49:54 +0000

    submit skeleton

---
+++ main.py
@@ -0,0 +1,91 @@
+import hashlib
+import hmac
+import json
+import os
+import pprint
+import urllib.request
+from datetime import datetime, timezone
+
+
+def main():
+    data = get_payload()
+    body = to_body(data)
+    response_data = send(body)
+    if response_data.get('success') != True:
+        pprint.pprint(response_data)
+        raise ValueError(f'Expected True for {response.data=} "success" key')
+    print(f"Receipt: {response_data['receipt']}")
+
+
+def get_payload():
+    now = datetime.now(timezone.utc)
+    return {
+        "email": get_env_var('EMAIL_ADDRESS'),
+        "name": get_env_var('NAME'),
+        "resume_link": get_env_var('RESUME_LINK'),
+        "timestamp": now.isoformat(timespec='milliseconds').replace('+00:00', 'Z'),
+        **get_github_info(),
+    }
+
+
+def get_github_info():
+    """see https://docs.github.com/en/actions/reference/workflows-and-actions/variables"""
+    repo = get_env_var("GITHUB_REPOSITORY")
+    host = get_env_var("GITHUB_SERVER_URL")
+    run_id = get_env_var("GITHUB_RUN_ID")
+    repo_url = f"{host}/{repo}"
+    return {
+        "repository_link": repo_url,
+        "action_run_link": f"{repo}/actions/runs/{run_id}",
+    }
+
+
+def get_env_var(key: str):
+    fail = True
+    try:
+        return os.environ[key]
+    except KeyError:
+        if fail:
+            raise
+        else:
+            return f"dummy-{key}"
+
+
+def to_body(data: dict) -> bytes:
+    return json.dumps(
+        data,
+        separators=(",", ":"),  # no whitespace
+        sort_keys=True,
+    ).encode("utf-8")
+
+
+def send(body):
+    signature = get_signature(body)
+    headers = {
+        "Content-Type": "application/json",
+        "X-Signature-256": f"sha256={signature}",
+    }
+    url = get_env_var('SUBMIT_URL')
+    req = urllib.request.Request(
+        url,
+        data=body,
+        headers=headers,
+        method="POST",
+    )
+
+    with urllib.request.urlopen(req) as response:
+        if response.status != 200:
+            raise ValueError(f'Expected 200, got {response.status=}')
+        raw = response.read().decode("utf-8")
+        return json.loads(raw)
+
+
+def get_signature(body: bytes):
+    # small script, only relevant here, so no dedicated settings/config module,
+    # but direct env access
+    secret = get_env_var("SIGNING_SECRET")
+    return hmac.new(secret.encode("utf-8"), body, hashlib.sha256).hexdigest()
+
+
+if __name__ == "__main__":
+    main()
